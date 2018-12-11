89b7b25e6e506d58f52d3519ebf7c1d9167ecb61
Radu Andra <radu-tudor.andra@student.manchester.ac.uk>
2018-12-11 22:26:33 +0000

    Test for merging

--- NLP/message_processing.py
+++ NLP/message_processing.py
@@ -1,3 +1,7 @@
+from dateutil.parser import parse
+
+important_words = ["appointment", "check medicine", ""]
+
 # Returns a list with the keywords found in the message
 def findKeyWords(message, key_words):
     message_words = message.split()
@@ -7,6 +11,14 @@ def findKeyWords(message, key_words):
             if message_word.lower() == key_word.lower():
                 message_key_words.append(key_word.lower())
     return message_key_words
+
+#Analyze the main keyword of the message
+def analyzeKeyWords(message_kew_words):
+    for key_word in message_kew_words:
+        if key_word in important_words:
+            makeAnswer(key_word)
+            break
+
 
 # Returns a string representing an answer to the message
 def makeAnswer(message):
@@ -20,4 +32,8 @@ def informationRequestedFromUser():
 # Uses some information to update the user
 def getRequestedInformation(message):
 
-#test
+#Check if we need any more information
+def checkInformationIntegrity(message_key_words):
+    for key_word in message_key_words:
+        if key_word == "appointment":
+
