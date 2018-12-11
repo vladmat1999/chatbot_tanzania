58add97e2a6e7ec5e8741af65a15cc845e83762e
Vlad <vlad-mihai.ene@student.manchester.ac.uk>
2018-12-11 20:58:08 +0000

    Initital commit

---
+++ NLP/message_processing.py
@@ -0,0 +1,14 @@
+# Returns a list with the keywords found in the message
+def findKeyWords(message, keywords):
+
+# Returns a string representing an answer to the message
+def makeAnswer(message):
+
+# Makes a request to receive some data
+def askForInformation(information):
+
+# Checks if a request for information was made
+def informationRequestedFromUser():
+
+# Uses some information to update the user
+def getRequestedInformation(message):
